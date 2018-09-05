277dfca38dfea5acdc7f156a55f6322ad9cf4f1b
aminda007 <aminda.14@cse.mrt.ac.lk>
2018-09-05 21:49:25 +0530

    add app name

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static('./dist/webvizualizer'));
+app.use(express.static('./dist/angular-tour-of-heroes'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/webvizualizer/index.html'));
+  res.sendFile(path.join('./dist/angular-tour-of-heroes/index.html'));
 });
 app.listen(process.env.PORT || 8080);
